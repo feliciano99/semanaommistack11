2e09f234a62c3387b7135c5a844370a4643b7ca3
Feliciano <feliciano99@users.noreply.github.com>
2020-03-31 17:24:29 -0300

    alterações frontend

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
+import '../../../src/global.css'
 
 import logoImg from '../../assets/logo.svg';
 
 
-export default function Profile(){
+export default function Profile() {
     return (
         <div className="profile-container">
             <header>
-                <img src={logoImg} alt="Be The Hero"/>
+                <img src={logoImg} alt="Be The Hero" />
                 <span>Bem vinda, APAD</span>
                 <Link className="button" to="incidents/new">
                     Cadastrar novo caso
                 </Link>
                 <button>
-                    <FiPower size={18} color="#E02041"/>
+                    <FiPower size={18} color="#E02041" />
                 </button>
             </header>
 
@@ -32,8 +33,22 @@ export default function Profile(){
 
                     <strong>VALOR:</strong>
                     <p>R$ 120,00</p>
-                    <button type="button"/>
-                    <FiTrash2 size={20} color="#A8A8B3"/>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>                    
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
                 </li>
                 <li>
                     <strong>CASO:</strong>
@@ -44,8 +59,9 @@ export default function Profile(){
 
                     <strong>VALOR:</strong>
                     <p>R$ 120,00</p>
-                    <button type="button"/>
-                    <FiTrash2 size={20} color="#A8A8B3"/>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
                 </li>
                 <li>
                     <strong>CASO:</strong>
@@ -56,20 +72,9 @@ export default function Profile(){
 
                     <strong>VALOR:</strong>
                     <p>R$ 120,00</p>
-                    <button type="button"/>
-                    <FiTrash2 size={20} color="#A8A8B3"/>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-                    <button type="button"/>
-                    <FiTrash2 size={20} color="#A8A8B3"/>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#A8A8B3" />
+                    </button>
                 </li>
             </ul>
         </div>
